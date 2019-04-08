9795b536aaf19755365d29e44e1e2f6d73830232
shakeeb <contacttoshakeeb@gmail.com>
2019-04-08 15:42:58 +0530

    Commened Godown

--- IMSBIZZ/Areas/MasterArea/Controllers/GoDownAPIController.cs
+++ IMSBIZZ/Areas/MasterArea/Controllers/GoDownAPIController.cs
@@ -15,15 +15,15 @@ namespace IMSBIZZ.Areas.MasterArea.Controllers
     [RoutePrefix("api/GoDown")]
     public class GoDownAPIController : ApiController
     {
-        private readonly IGodownService _godownService;
+        //private readonly IGodownService _godownService;
 
-        /// <summary>
-        /// Constructor to inject Batch Service
-        /// </summary>
-        /// <param name="batchService">Batch Service Instance</param>
-        public BatchAPIController(IGodownService godownService)
-        {
-            _godownService = godownService;
-        }
+        ///// <summary>
+        ///// Constructor to inject Batch Service
+        ///// </summary>
+        ///// <param name="batchService">Batch Service Instance</param>
+        //public BatchAPIController(IGodownService godownService)
+        //{
+        //    _godownService = godownService;
+        //}
     }
 }
